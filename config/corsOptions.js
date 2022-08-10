191df7ed0cfbf86cf6b4fc5e6ef0a016b9d06751
0xchaos <62848968+Am0stafa@users.noreply.github.com>
2022-08-10 20:43:52 +0200

    Update corsOptions.js
    to set for production

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -7,8 +7,8 @@ const whitelist = [
 
 const corsOptions = {
     origin: (origin, callback) => {
-            //! in deployment we must remove !origin !!
-        if (whitelist.indexOf(origin) !== -1 || !origin) {
+            //! in deployment we must remove !origin !! in deployment (whitelist.indexOf(origin) !== -1 || !origin) 
+        if (whitelist.indexOf(origin) !== -1) {
             callback(null, true)
         } else {
             callback(new Error('Not allowed by CORS'));
